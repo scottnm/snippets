2dba2cd2cc6f4943a7071b1df7c66beaefbad67f
Scott Munro <scottnmunro@gmail.com>
2019-10-06 20:07:46 -0700

    alternate mylast solution

--- haskell/99p/02.hs
+++ haskell/99p/02.hs
@@ -1,4 +1,7 @@
-myButLast :: [a] -> a
-myButLast [] = error "not enough elements provided!"
-myButLast [_] = error "not enough elements provided!"
-myButLast list = list !! ((length list) - 2)
+myButLastA :: [a] -> a
+myButLastA [] = error "not enough elements provided!"
+myButLastA [_] = error "not enough elements provided!"
+myButLastA list = list !! ((length list) - 2)
+
+myButLastB :: [a] -> a
+myButLastB = last . init
