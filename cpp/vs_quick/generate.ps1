2b05e4f7af714b87ce21023d45a5440f013933a3
Scott Munro <scottnmunro@gmail.com>
2020-04-25 20:16:59 -0700

    replace uuids

--- cpp/vs_quick/generate.ps1
+++ cpp/vs_quick/generate.ps1
@@ -11,9 +11,7 @@ cp -rec $PSScriptRoot\template_project $ProjectFolder\$Name
 pushd $ProjectFolder\$Name
 
 dir vs_quick.* | Rename-Item -NewName { $_.name -replace "vs_quick",$Name }
-$files = dir "$Name.*"
-
-echo "Files: $files"
+$files = dir "$Name.*","build.ps1"
 
 function ReplaceInFiles {
     Param(
@@ -26,10 +24,18 @@ function ReplaceInFiles {
 }
 
 # REPLACE REFERENCES TO vs_quick with $Name
-ReplaceInFiles -FileSet $Files -Old "vs_quick" -New $Name
-ReplaceInFiles -FileSet $Files -Old "vsquick" -New $Name
+ReplaceInFiles -FileSet $files -Old "vs_quick" -New $Name
+ReplaceInFiles -FileSet $files -Old "vsquick" -New $Name
 
 # REPLACE GUIDS
+$files |
+    sls "\w{8}-\w{4}-\w{4}-\w{4}-\w{12}" |
+    %{ $_.Matches.Groups[0].Value } |
+    sort |
+    Get-Unique -AsString |
+    %{
+        ReplaceInFiles -FileSet $files -Old $_ -New (new-guid)
+    }
 
 <#
 $files |
